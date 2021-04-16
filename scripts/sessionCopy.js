ddd0a6949a1b527ca4c229be8341845950d4e6a0
borganstein <github@transcendingdigital.space>
2021-04-16 19:05:16 +1000

    major changes around animation for copying session ID

--- scripts/sessionCopy.js
+++ scripts/sessionCopy.js
@@ -1,12 +1,32 @@
-function copysessionIDfunction() {
-    navigator.clipboard.writeText('05577626bd543fbd9cc17239cdaf1e6c6e402e4f3ef4eb964b72f3c851f1d91c30')
-    var tooltip = document.getElementById("session-id");
-    tooltip.innerHTML = "Copied!";
+function sessionIDCopy() {
+  const modalDiv  = document.querySelector('#session-modal-background');
+  const bodyContainer = document.querySelector("body"); // selects Body container
+
+  navigator.clipboard.writeText('05577626bd543fbd9cc17239cdaf1e6c6e402e4f3ef4eb964b72f3c851f1d91c30');
+  modalDiv.style.display = "flex";
+  // bodyContainer.classList.add('interaction-disabled');
+  bodyContainer.classList.add('stop-scroll');
+
+}
+/* --------------------------------------*/
+function hoverFunc() {
+  const buttonText = document.querySelector("#session-button-text");
+  const hoverText = document.querySelector('#session-button-hover-text');
+  buttonText.classList.add('display-none');
+  hoverText.classList.remove('display-none');
 }
 
-
-/* --------------------------------------*/
 function outFunc() {
-  var tooltip = document.getElementById("session-id");
-  tooltip.innerHTML = "Copy to clipboard";
+  const buttonText = document.querySelector("#session-button-text");
+  const hoverText = document.querySelector('#session-button-hover-text');
+  buttonText.classList.remove('display-none');
+  hoverText.classList.add('display-none');
+}
+
+function closeModal() {
+  const bodyContainer = document.querySelector("body"); // selects Body container
+  const modalDiv  = document.querySelector('#session-modal-background');
+  bodyContainer.classList.remove('interaction-disabled');
+  bodyContainer.classList.remove('stop-scroll');
+  modalDiv.style.display = "none";
 }
