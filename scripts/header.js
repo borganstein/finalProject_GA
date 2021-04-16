5d3e2a3e2398a7a3ddc22ac20658731aa0c37212
borganstein <github@transcendingdigital.space>
2021-04-16 15:07:11 +1000

    Added comments for easer reading

--- scripts/header.js
+++ scripts/header.js
@@ -9,18 +9,20 @@ window.onscroll = () => {
   const menuButton = document.getElementById('menu-button');
   const headerTitle = document.getElementById('header-title');
   const header = document.getElementById('header');
-  const heightOfHero = document.getElementById('hero-cover').clientHeight;
+  // const heightOfHero = document.getElementById('hero-cover').clientHeight;
 
-  if (window.scrollY > heightOfHero) {
+  if (window.scrollY) {
     menuButton.classList.remove('menu-icon-white')
     menuButton.classList.add('menu-icon-blue')
     headerTitle.classList.remove('display-none')
     header.classList.add('header-background-opacity')
-    
+    header.classList.add('shadow');
+
   } else {
     menuButton.classList.remove('menu-icon-blue')
     menuButton.classList.add('menu-icon-white')
     headerTitle.classList.add('display-none')
     header.classList.remove('header-background-opacity')
+    header.classList.remove('shadow');
   }
 }
