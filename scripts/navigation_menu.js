db2f8038b3331271e61720feb201ad7c5338fb59
borganstein <github@transcendingdigital.space>
2021-04-26 17:54:51 +1000

    fixed header.js with id change;

--- scripts/navigation_menu.js
+++ scripts/navigation_menu.js
@@ -1,12 +1,15 @@
+
+const mainContainer = document.querySelector("main"); // selects Main container
+const bodyContainer = document.querySelector("body"); // selects Body container
+const menuLink = document.querySelectorAll(".menu-link"); //querySelectorAll returns a list
+const socialLink = document.querySelectorAll(".social-link"); //querySelectorAll returns a list
+const sideMenu = document.querySelector("#mySidenav"); // selects element with ID mySidenav
+const menuButton = document.querySelector("#menuButton");
+const closeButton = document.querySelector('#closeButton');
+
 // ####################### Navigation Menu JS #####################
 function openNav() {
   // setting variables for use within function
-  const mainContainer = document.querySelector("main"); // selects Main container
-  const bodyContainer = document.querySelector("body"); // selects Body container
-  const menuLink = document.querySelectorAll(".menu-link"); //querySelectorAll returns a list
-  const socialLink = document.querySelectorAll(".social-link"); //querySelectorAll returns a list
-  const sideMenu = document.querySelector("#mySidenav"); // selects element with ID mySidenav
-
   sideMenu.classList.remove('hidden'); // removes .hidden class from sidemenu
   sideMenu.style.width = "300px"; // changes width to 60%; can be done without but this aids in having nice animation
   mainContainer.classList.add('interaction-disabled'); //disables pointer-events
@@ -17,15 +20,7 @@ function openNav() {
 
 };
 
-const menuButton = document.querySelector("#menuButton");
-menuButton.addEventListener('click', openNav);
-
 function closeNav() {
-  const mainContainer = document.querySelector("main"); // selects Main container
-  const bodyContainer = document.querySelector("body"); // selects Body container
-  const menuLink = document.querySelectorAll(".menu-link") //querySelectorAll returns a list
-  const socialLink = document.querySelectorAll(".social-link") //querySelectorAll returns a list
-  const sideMenu = document.querySelector("#mySidenav"); // selects element with ID mySidenav
 
   sideMenu.classList.add('hidden') // hides the menu
   sideMenu.style.width = "0"; // makes the width 0; can be done without but this makes for a nice animation
@@ -36,9 +31,12 @@ function closeNav() {
   menuLink.forEach(element => element.tabIndex = "-1"); //uses forEach to iterate through array to add tabIndex=-1 to all elements with class .menu-link
 };
 
-const closeButton = document.querySelector('#closeButton');
-const sideMenu = document.querySelector("#mySidenav"); // selects element with ID mySidenav
+//################## Click Listener ###################
+
+menuButton.addEventListener('click', openNav);
 closeButton.addEventListener("click", closeNav);
 sideMenu.addEventListener('click', closeNav);
+
+//#####################################################
 
 // ----------------------------------------------------------------
