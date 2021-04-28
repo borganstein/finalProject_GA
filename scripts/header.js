2bf7d335c024d04a25ab1df84039b0d011f2c56d
borganstein <github@transcendingdigital.space>
2021-04-28 22:14:10 +1000

    fixed animation across pages; added social button animation;

--- scripts/header.js
+++ scripts/header.js
@@ -5,31 +5,35 @@
 // ############################################################################
 
 window.onscroll = () => {
-  const menuButton = document.querySelector('#menuButton');
+  const menuIcon = document.querySelector('#menuIcon');
   const headerTitle = document.querySelector('#headerTitle');
   const header = document.querySelector('#header');
   const heroH1 = document.querySelector('.hero-h1');
   const headerArea = document.querySelector('.header-menuButton-title');
 
   if (window.scrollY) {
-    menuButton.classList.remove('menu-icon-white');
-    menuButton.classList.add('menu-icon-blue');
+    menuIcon.classList.remove('menu-icon-white');
+    menuIcon.classList.add('menu-icon-blue');
+    //
     headerArea.classList.remove('hidden');
     headerTitle.classList.add('transformLarge')
     headerTitle.classList.remove('hidden');
     header.classList.add('header-background-opacity');
     header.classList.add('shadow');
+    //
     heroH1.classList.remove('transformLarge');
-    heroH1.classList.add('scaleTransformSmall');
+    heroH1.classList.add('transformSmall');
     heroH1.classList.add('hidden');
 
   } else {
-    menuButton.classList.remove('menu-icon-blue');
-    menuButton.classList.add('menu-icon-white');
+    menuIcon.classList.remove('menu-icon-blue');
+    menuIcon.classList.add('menu-icon-white');
+    //
     headerArea.classList.add('hidden');
     headerTitle.classList.add('hidden');
     header.classList.remove('header-background-opacity');
     header.classList.remove('shadow');
+    //
     heroH1.classList.remove('transformSmall');
     heroH1.classList.add('transformLarge');
     heroH1.classList.remove('hidden');
