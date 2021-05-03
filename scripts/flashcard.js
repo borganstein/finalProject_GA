d9cee6d674f22fd2a8b84a110d1fd2bd27512698
borganstein <github@transcendingdigital.space>
2021-05-04 01:22:51 +1000

    New flashcard animation;

--- scripts/flashcard.js
+++ scripts/flashcard.js
@@ -8,26 +8,37 @@ const germanList = ["der Stern", "der Berg", "der Stuhl", "der Wissenschaftler"]
 const englishList = ["The Star", "The Mountain", "The Chair", "The Scientist"]; //obv. the matched english set of words
 let englishList_randomNumber = []; //an object to store the randomNumber generated to link german to english lists
 
+const flashcardBack = document.querySelector("#flashcardEnglish");
+const flashcardFront = document.querySelector("#flashcardGerman");
+const flashcardGermanText = document.querySelector("#flashcardGermanText")
+const flashcardEnglishText = document.querySelector("#flashcardEnglishText")
+const flipButton = document.querySelector("#flipButton");
 
 function flipcardButton() {
-  //declaring variables for readability of code
-  //
-  const flashcardBack = document.querySelector("#flashcardEnglish");
-  const flashcardFront = document.querySelector("#flashcardGerman");
-  const flashcardGermanText = document.querySelector("#flashcardGermanText")
-  const flashcardEnglishText = document.querySelector("#flashcardEnglishText")
-  //
   //
   let randomNumber = Math.floor(Math.random() * germanList.length); //create a random between 0 and the length of the german list
-  flashcardGermanText.innerHTML = germanList[randomNumber]; //the location of the final output
+  flashcardGermanText.innerText = germanList[randomNumber]; //the location of the final output
   //
   let englishList_randomNumber = englishList[randomNumber]; // store that randomNumber
-  flashcardEnglishText.innerHTML = englishList_randomNumber; //the location of the english output
+  flashcardEnglishText.innerText = englishList_randomNumber; //the location of the english output
 };
 
-// ########## Click Events ###########
+
+function flipCard() {
+  flashcardFront.classList.add('hidden');
+  flashcardBack.classList.remove('hidden');
+  flashcardBack.classList.add('transform-rotate')
+};
+
+function flipCardBack() {
+  flashcardFront.classList.remove('hidden');
+  flashcardBack.classList.add('hidden');
+  flashcardFront.classList.add('transform-rotate')
+};
+// ########## Events ###########
 //
-const flipButton = document.querySelector("#flipButton");
 flipButton.addEventListener('click', flipcardButton);
+// flashcardFront.addEventListener('click', flipCard);
+// flashcardBack.addEventListener('click', flipCardBack);
 //
 // ############################################################################
