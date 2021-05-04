57a2cac543e6c6b02be308ae47ba9214eac5a214
borganstein <github@transcendingdigital.space>
2021-05-04 15:58:56 +1000

    fixed session page/buttons and js animation

--- scripts/sessionCopy.js
+++ scripts/sessionCopy.js
@@ -4,37 +4,29 @@
 //
 //#############################################################################
 
+const buttonText = document.querySelector("#session-button-text");
+const hoverText = document.querySelector('#session-button-hover-text');
+const sessionButton = document.querySelector("#sessionButton");
 
 function sessionIDCopy() {
-  const modalDiv  = document.querySelector('.modal-background');
-  const bodyContainer = document.querySelector("body"); // selects Body container
   navigator.clipboard.writeText('05577626bd543fbd9cc17239cdaf1e6c6e402e4f3ef4eb964b72f3c851f1d91c30');
-  modalDiv.style.display = "flex";
-  bodyContainer.classList.add('stop-scroll');
+  hoverText.classList.remove('hidden');
+  hoverText.innerText = "Copied!";
+  hoverText.style.backgroundColor = "#268bcc";
+  hoverText.style.color = "#f5f5f5";
   setTimeout(function() {
-    bodyContainer.classList.remove('stop-scroll');
-    modalDiv.style.display = "none";
-  }, 2500);
+    hoverText.innerText = "Copy";
+    hoverText.style.backgroundColor = "#f5f5f5";
+    hoverText.style.color = "#268bcc";
+  }, 1000);
 };
 
 function hoverFunc() {
-  const buttonText = document.querySelector("#session-button-text");
-  const hoverText = document.querySelector('#session-button-hover-text');
-  buttonText.classList.add('display-none');
-  hoverText.classList.remove('display-none');
-  setTimeout(function() {
-      buttonText.classList.remove('display-none');
-      hoverText.classList.add('display-none');
-    }, 1000);
+  hoverText.classList.remove('hidden');
 };
 
-function closeModal() {
-  const bodyContainer = document.querySelector("body"); // selects Body container
-  const modalDiv  = document.querySelector('#modalBackground');
-  // bodyContainer.classList.remove('interaction-disabled');
-  bodyContainer.classList.remove('stop-scroll');
-  modalDiv.style.display = "none";
-
+function exitHoverFunc() {
+  hoverText.classList.add('hidden');
 }
 
 //#############################################################################
@@ -42,25 +34,13 @@ function closeModal() {
 //                Click and Mouse Events
 //
 //#############################################################################
-
-const sessionButton = document.querySelector("#sessionButton");
-const closeModalButton = document.querySelector("#closeModalButton");
-const modalBackground = document.querySelector("#modalBackground");
 //
 sessionButton.addEventListener('click', sessionIDCopy);
 sessionButton.addEventListener('mouseenter', hoverFunc);
-modalBackground.addEventListener('click', closeModal);
+sessionButton.addEventListener('mouseleave', exitHoverFunc);
 
 //#############################################################################
 //
 //                    Graveyard
 //
 //#############################################################################
-
-// set a timeout instead
-// function outFunc() {
-//   const buttonText = document.querySelector("#session-button-text");
-//   const hoverText = document.querySelector('#session-button-hover-text');
-//   buttonText.classList.remove('display-none');
-//   hoverText.classList.add('display-none');
-// }
